731136fbde8113a5b02af54bf0139b2c463a0b3b
tionry <tionry@163.com>
2014-07-11 16:16:59 +0800

    no message

--- js/js_1.js
+++ js/js_1.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 				var index=0;
 				var slideFlag = true;
 				var length=$(".roll-news-image img").length;
+				console.log(page_num);
 				comments_json(localStorage.page_num);
 				function showImg(i){
 					$(".roll-news-image img")
