72f8d9ff27f8b296d96f64eba449796a0da5cd33
tionry <tionry@163.com>
2014-07-11 20:47:22 +0800

    no message

--- js/js_3.js
+++ js/js_3.js
@@ -7,7 +7,7 @@
 			var b = $('#span_currcommenttotal0');
 			b[0].innerHTML = (page_num-1)*3 + 1;
 			var c = $('#span_currcommenttotal1');
-			c[0].innerHTML = (page_num*3-1) + data.comments.length;					
+			c[0].innerHTML = (page_num-1)*3 + data.comments.length;					
 			for(var a = 0;a < data.comments.length;a++){				
 				$('div.comments').append(
 				"<div class = 'comment'><div class ='commentcon'><div class = 'userPhoto'><img width = '50' height = '50' src = '"+data.comments[a].pic+"'></img></div><div class = 'bar'><a href='#blank' style = 'text-decoration: none''>"+data.comments[a].name+"</a></div><div class = 'con'><div class = 'text'><p>" + data.comments[a].content+"</p><br></div></div></div></div>");
