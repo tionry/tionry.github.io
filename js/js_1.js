6042db1b60d6ade5f6a993e1cda1bbcab079de85
tionry <tionry@163.com>
2014-07-11 20:58:20 +0800

    no message

--- js/js_1.js
+++ js/js_1.js
@@ -2,23 +2,23 @@
 						
 				var index=0;
 				var slideFlag = true;
-				var length=$(".roll-news-image img").length;				
+				var length=$('.roll-news-image img').length;				
 				function showImg(i){
-					$(".roll-news-image img")
-					.eq(i).stop(true,true).fadeIn(1800)
+					$('.roll-news-image img')
+					.eq(i).stop(true,true).fadeIn('slow')
 					.siblings("img").hide();
 
-					$(".roll-news-index li").removeClass("roll-news-index-hover");
-					$(".roll-news-index li").eq(i).addClass("roll-news-index-hover");
+					$('.roll-news-index li').removeClass('roll-news-index-hover');
+					$('.roll-news-index li').eq(i).addClass('roll-news-index-hover');
 
-					$(".roll-news-title a")
-					.eq(i).stop(true,true).fadeIn(1800)
-					.siblings("a").hide();
+					$('.roll-news-title a')
+					.eq(i).stop(true,true).fadeIn('slow')
+					.siblings('a').hide();
 				}
 				showImg(index);
 				
-				$(".roll-news-index li").click(function(){
-					index = $(".roll-news-index li").index(this);
+				$('.roll-news-index li').click(function(){
+					index = $('.roll-news-index li').index(this);
 					showImg(index);
 					slideFlag = false;
 				});	
