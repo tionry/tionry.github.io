b6274bb0d1c0786958bdf83c6c4fa9d59df89d79
tionry <tionry@163.com>
2014-07-11 10:27:55 +0800

    no message

--- js/js_3.js
+++ js/js_3.js
@@ -4,9 +4,9 @@
 		
 		function processData(data) {
 			var c  =$('#span_currcommenttotal0');
-			c[0].innerHTML = (pagenum -1)*3;
+			c[0].innerHTML = (page_num -1)*3;
 			var b  =$('#span_currcommenttotal1');
-			b[0].innerHTML = (pagenum -1)*3 + data.comments.length;
+			b[0].innerHTML = (page_num -1)*3 + data.comments.length;
 			for(var a = 0;a < data.comments.length;a++){				
 				$('div.comments').append(
 				"<div class = 'comment'><div class ='commentcon'><div class = 'userPhoto'><img width = '50' height = '50' src = '"+data.comments[a].pic+"'></img></div><div class = 'bar'><a href='#blank' style = 'text-decoration: none''>"+data.comments[a].name+"</a></div><div class = 'con'><div class = 'text'><p>" + data.comments[a].content+"</p><br></div></div></div></div>");
