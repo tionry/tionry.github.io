5da3ea6fd1f4238ae4cb5bf31c9f40e64fc9b1e3
tionry <tionry@163.com>
2014-07-11 20:44:27 +0800

    no message

--- js/js_3.js
+++ js/js_3.js
@@ -2,8 +2,12 @@
 		$('div.comment').remove();
 		var url = '/comments'+page_num+'.json';
 		localStorage.page_num = page_num; 
+		
 		function processData(data) {
-			console.log(data.comments.length);	
+			var b = $('#span_currcommenttotal0');
+			b[0].innerHTML = page_num*3 + 1;
+			var c = $('#span_currcommenttotal1');
+			c[0].innerHTML = page_num*3 + data.comments.length;					
 			for(var a = 0;a < data.comments.length;a++){				
 				$('div.comments').append(
 				"<div class = 'comment'><div class ='commentcon'><div class = 'userPhoto'><img width = '50' height = '50' src = '"+data.comments[a].pic+"'></img></div><div class = 'bar'><a href='#blank' style = 'text-decoration: none''>"+data.comments[a].name+"</a></div><div class = 'con'><div class = 'text'><p>" + data.comments[a].content+"</p><br></div></div></div></div>");
