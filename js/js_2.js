3d75fb2e1b1dbe594c9799bd58d58a74bf756219
tionry <tionry@163.com>
2014-07-11 10:07:30 +0800

    no message

--- js/js_2.js
+++ js/js_2.js
@@ -1,38 +1,37 @@
-	function() {
-		var url = '/data.json';
 
-		function processData(data) {
-			$('img').each(function(index,m){
-			var c = $(m);
-			if(index < 3) c[0].setAttribute("src",data.news[index].pic_href);			
-			})
-						
-			$('a').each(function(index,m){
-			var c = $(m);
-			if(index>0 && index < 4){
-			c[0].innerHTML = data.news[index - 1].new_title;
-			c[0].setAttribute("href",data.news[index - 1].news_href);
-			}
-			})
-		  }
+	var url = '/data.json';
 
-		  function handler() {
-			if (this.readyState == this.DONE) {
-			  if (this.status == 200) {
-				try {
-				  processData(JSON.parse(this.responseText));
-				} catch(ex) {
-				  console.log(ex.message);
-				}
-			  }
+	function processData(data) {
+		$('img').each(function(index,m){
+		var c = $(m);
+		if(index < 3) c[0].setAttribute("src",data.news[index].pic_href);			
+		})
+					
+		$('a').each(function(index,m){
+		var c = $(m);
+		if(index>0 && index < 4){
+		c[0].innerHTML = data.news[index - 1].new_title;
+		c[0].setAttribute("href",data.news[index - 1].news_href);
+		}
+		})
+	  }
+
+	  function handler() {
+		if (this.readyState == this.DONE) {
+		  if (this.status == 200) {
+			try {
+			  processData(JSON.parse(this.responseText));
+			} catch(ex) {
+			  console.log(ex.message);
 			}
 		  }
+		}
+	  }
 
-		  function ajax() {
-			var client = new XMLHttpRequest();
-			client.onreadystatechange = handler;
-			client.open('GET', url,true);
-			client.send();
-		  }
-		   ajax();
-		}();
+	  function ajax() {
+		var client = new XMLHttpRequest();
+		client.onreadystatechange = handler;
+		client.open('GET', url,true);
+		client.send();
+	  }
+	   ajax();
